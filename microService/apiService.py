45db8d4c4ecb984605965edb48a78bf3205afe8d
Amir Katz <amirkatzster@gmail.com>
2019-03-09 23:47:42 +0200

    react

--- microService/apiService.py
+++ microService/apiService.py
@@ -38,7 +38,7 @@ api.add_resource(actionService, '/action/<string:command>')
 api.add_resource(sayService, '/say/<string:text>')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=8080,debug=True)
 
 
 
---
+++ web-app/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+	  super(props);
+	  this.state = {value: ''};
+	  this.handleChange = this.handleChange.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+	      this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+	alert('A name was submitted: ' + this.state.value);
+	event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            בוב
+          </p>
+	  <form onSubmit={this.handleSubmit}>
+	     <label>
+	       תגיד:
+	          <input type="text" value={this.state.value} onChange={this.handleChange} />
+	     </label>
+	     <input type="submit" value="שלח" />
+	  </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
